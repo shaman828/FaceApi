842fe556584eb1edd54c549fd283a969b7b9056f
chenjing <shaman828@163.com>
2016-09-09 18:23:20 +0800

    faceapi

--- FaceApiWpf/ViewModel/FaceApiHelper.cs
+++ FaceApiWpf/ViewModel/FaceApiHelper.cs
@@ -3,39 +3,228 @@
  * FileName:    FaceApiHelper
  * FrameWork:   4.5.2
  * CreateDate:  2016/9/9 11:24:33
- * Description:  
- * 
+ * Description: 微软感知服务--人脸识别
+ *
  * ************************************************************************************************/
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.ProjectOxford.Face;
 
 namespace FaceApiWpf.ViewModel
 {
     public class FaceApiHelper
     {
-        private FaceServiceClient faceServiceClient;
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// <remarks>
+        /// facelistId:为空时,自动创建Guid
+        /// </remarks>
+        /// </summary>
+        /// <param name="serviceKey">服务key</param>
+        /// <param name="facelistId">容器Id</param>
+        public FaceApiHelper(string serviceKey, string facelistId = "")
+        {
+            ServiceKey = serviceKey;
+            if (string.IsNullOrEmpty(facelistId))
+            {
+                facelistId = Guid.NewGuid().ToString();
+            }
+            FaceListId = facelistId;
+            faceServiceClient = new FaceServiceClient(ServiceKey);
+        }
+
+        #endregion Constructors
+
+        #region Fields
+
+        private readonly FaceServiceClient faceServiceClient;
+
+        private string faceListId = string.Empty;
         private string serviceKey = string.Empty;
 
+        #endregion Fields
+
+        #region Properties Public
+
+        public string FaceListId
+        {
+            get { return faceListId; }
+            private set
+            {
+                faceListId = value;
+            }
+        }
+
+        /// <summary>
+        /// 当前服务key
+        /// </summary>
         public string ServiceKey
         {
             get { return serviceKey; }
             private set { serviceKey = value; }
         }
 
+        #endregion Properties Public
 
-        public FaceApiHelper(string serviceKey)
+        #region Methods Public
+        /// <summary>
+        /// 两图片比较
+        /// </summary>
+        /// <param name="srcImg">原图片</param>
+        /// <param name="targetImg">目标图片</param>
+        /// <returns></returns>
+        public async Task<double> FindSimilar(string srcImg, string targetImg)
         {
-            ServiceKey = serviceKey;
+            var src = ImgToFaceList(srcImg);
+            Task<Guid> target = UploadTargetImg(targetImg);
+            var tasks = new List<Task> { src, target };
+            Task.WaitAll(tasks.ToArray());
+            var targetFaceId = target.Result;
+
+            return await FindSimilar(targetFaceId);
+        }
+        /// <summary>
+        /// 根据目标图片faceid比较
+        /// </summary>
+        /// <param name="targetFaceId"></param>
+        /// <returns></returns>
+        public async Task<double> FindSimilar(Guid targetFaceId)
+        {
+            double confidenceet = 0;
+            if (targetFaceId != Guid.Empty)
+            {
+                var ret = await faceServiceClient.FindSimilarAsync(targetFaceId, FaceListId, 3);
+                if (ret.Any())
+                {
+                    confidenceet = ret.Max(x => x.Confidence);
+                }
+            }
+
+            return confidenceet;
         }
 
-        public double CompareFace(string imgPath1,string imgPath2,string faceListId="")
+        /// <summary>
+        /// 上传文件夹中所有图片
+        /// </summary>
+        /// <param name="folder"></param>
+        public async void FolderImgToFaceList(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                return;
+            }
+            var pattern = @"\.(jpg|png|JPG|PNG)$";
+            var imgs = Directory.EnumerateFiles(folder, pattern, SearchOption.AllDirectories);
+            List<Task> tasks = new List<Task>();
+            if (imgs.Any())
+            {
+
+                foreach (var img in imgs)
+                {
+                    tasks.Add(Task.Factory.StartNew(async (obj) =>
+                    {
+                        var imgPath = obj as string;
+                        await ImgToFaceList(imgPath);
+                    }, img));
+                }
+            }
+            if (tasks.Any())
+            {
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        /// <summary>
+        /// 上传图片到faceList
+        /// </summary>
+        /// <param name="imgPath"></param>
+        /// <returns></returns>
+        public async Task<AddPersistedFaceResult> ImgToFaceList(string imgPath)
+        {
+            if (!File.Exists(imgPath) || string.IsNullOrEmpty(imgPath))
+            {
+                return null;
+            }
+            using (var stream = File.OpenRead(imgPath))
+            {
+                AddPersistedFaceResult ret = await faceServiceClient.AddFaceToFaceListAsync(faceListId, stream);
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// 获取当ServiceKey下所有FaceList
+        /// </summary>
+        public async Task<FaceListMetadata[]> GetFaceLists()
+        {
+            var list = await faceServiceClient.ListFaceListsAsync();
+            return list;
+        }
+
+        /// <summary>
+        /// 上传目标图片
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        public async Task<Guid> UploadTargetImg(string img)
+        {
+            var imgFaceId = Guid.Empty;
+            if (File.Exists(img))
+            {
+                using (var stream = File.OpenRead(img))
+                {
+                    Face[] faces = await faceServiceClient.DetectAsync(stream);
+                    if (faces.Any())
+                    {
+                        imgFaceId = faces.FirstOrDefault().FaceId;
+                    }
+                }
+            }
+            return imgFaceId;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="listId"></param>
+        public void DeleteFaceListByFaceId(string listId)
         {
             //todo
-            return 0;
         }
+        /// <summary>
+        /// 删除所有FaceList(当前faceList除外)
+        /// </summary>
+        public async void ClearnFaceList()
+        {
+            FaceListMetadata[] faceList = await GetFaceLists();
+            if (faceList.Any())
+            {
+                foreach (var item in faceList.AsParallel())
+                {
+                    DeleteFaceListByFaceId(item.FaceListId);
+                }
+            }
+        }
+
+        public async Task<FaceList> GetFaceListInfoById(string listId)
+        {
+            return await faceServiceClient.GetFaceListAsync(listId);
+        }
+        /// <summary>
+        /// 获取当前facelist信息
+        /// </summary>
+        /// <returns></returns>
+        public FaceList GetCurrentFaceListInfo()
+        {
+            FaceList info = GetFaceListInfoById(FaceListId).Result;
+            return info;
+        }
+
+        #endregion Methods Public
     }
 }
